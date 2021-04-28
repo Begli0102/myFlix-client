bb9d49094877119fa0914778f34ab2161c29c538
Begli0102 <begli.kaka@mail.ru>
2021-04-28 18:04:54 +0200

    made some changes

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import axios from 'axios';
+
 import  {MovieCard}  from '../movie-card/movie-card';
 import  {MovieView} from '../movie-view/movie-view';
 
@@ -7,28 +9,23 @@ export class MainView extends React.Component {
   constructor() {
     super();
     this.state = {
-      movies:
-        [
-        { _id: 1, Title: 'The Godfather',
-         Description:" The story, spanning from 1945 to 1955, chronicles the Corleone family under patriarch Vito Corleone (Brando), focusing on the transformation of his youngest son, Michael Corleone (Pacino), from reluctant family outsider to ruthless mafia boss.",
-          ImagePath: "https://m.media-amazon.com/images/M/MV5BM2MyNjYxNmUtYTAwNi00MTYxLWJmNWYtYzZlODY3ZTk3OTFlXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_UY268_CR3,0,182,268_AL_.jpg",
-          Director:"Francis Ford Coppola",
-          Genre:"Crime"},
-
-        { _id: 2, Title: "Jurassic Park",
-         Description: "Update  It is an American science fiction media franchise centered on a disastrous attempt to create a theme park of cloned dinosaurs.",
-         ImagePath: "https://m.media-amazon.com/images/M/MV5BMjM2MDgxMDg0Nl5BMl5BanBnXkFtZTgwNTM2OTM5NDE@._V1_UX182_CR0,0,182,268_AL_.jpg",
-         Director:"Steven Allan Spielberg",
-         Genre:"Science fiction"},
-
-        { _id: 3, Title: "Close Encounters of the Third Kind",
-         Description:"It tells the story of Roy Neary, an everyday blue-collar worker in Indiana, whose life changes after an encounter with an unidentified flying object.",
-         ImagePath:  "https://m.media-amazon.com/images/M/MV5BMjM1NjE5NjQxN15BMl5BanBnXkFtZTgwMjYzMzQxMDE@._V1_UX182_CR0,0,182,268_AL_.jpg",
-         Director:"Steven Allan Spielberg",
-         Genre:"Science fiction"}
-        ],
+      movies:[],
       selectedMovie: null
     };
+  }
+     
+  //using axios method to fetch movies from heroku
+  
+  componentDidMount(){
+    axios.get('https://myflix01025.herokuapp.com/movies')
+      .then(response => {
+        this.setState({
+          movies: response.data
+        });
+      })
+      .catch(error => {
+        console.log(error);
+      });
   }
 
   setSelectedMovie(newSelectedMovie) {
@@ -52,6 +49,9 @@ export class MainView extends React.Component {
            }
          </div>
   );
+}
+componentDidMount() {
+  // code executed right after the component is added to the DOM
 }
 }
 export default MainView;
